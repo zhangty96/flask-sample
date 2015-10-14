e5e1be634c5ecdce07f259a12ed163b0afc7b14a
Walter Sagehorn <walter@wireless-10-147-81-142.public.utexas.edu>
2015-10-14 18:49:38 -0500

    fixed and tested

--- my_app.py
+++ my_app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 from flask.ext.mobility import Mobility
 from flask.ext.mobility.decorators import mobile_template
+
+from forms import NameForm
 
 app = Flask(__name__)
 Mobility(app)
@@ -8,19 +10,19 @@ Mobility(app)
 app.secret_key = 'CHANGE_ME'
 
 ### ROUTING ###
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 @mobile_template('{mobile/}index.html')
 def index(template):
-    form = NameForm(request.form)
-    if request.method == 'POST' and form.validate():
-    	session["name"] = form.first_name.data
-    return render_template(template, form=form)
+	form = NameForm(request.form)
+	if request.method == 'POST' and form.validate():
+		session["name"] = form.name.data
+	return render_template(template, form=form)
 
 #clears the session and then redirects back to index
 @app.route('/logout')
-def index():
+def logout():
 	session["name"] = ""
-    return redirect(url_for('index'))
+	return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
